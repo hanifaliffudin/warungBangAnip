8a7288d40c862eb038a2a74be06da1a9072eb0bb
hanif aliffudin <hanifaliffudin@gmail.com>
2020-12-09 12:28:31 +0700

    first commit

--- liff-starter.js
+++ liff-starter.js
@@ -21,8 +21,13 @@ function getProfile() {
   liff.getProfile()
     .then(profile => {
       const name = profile.displayName
+      displayName(name)
     })
     .catch((err) => {
       console.log('error', err);
     })
+}
+
+function displayName(name) {
+  document.getElementById('nama').innerText = name
 }
