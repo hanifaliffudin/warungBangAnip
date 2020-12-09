ea5aa6d6b9adc1b79d6efdf7588a0989accc3f7c
hanif aliffudin <hanifaliffudin@gmail.com>
2020-12-09 12:44:09 +0700

    first commit

--- liff-starter.js
+++ liff-starter.js
@@ -21,10 +21,23 @@ function getProfile() {
   liff.getProfile()
     .then(profile => {
       const name = profile.displayName
-      document.getElementById('nama').textContent = name
+      document.getElementById('nama').textContent = profile.displayName
     })
     .catch((err) => {
       console.log('error', err);
     })
 }
 
+document.getElementById('login-btn').addEventListener('click', function () {
+  if (!liff.isLoggedIn()) {
+    // set `redirectUri` to redirect the user to a URL other than the front page of your LIFF app.
+    liff.login();
+  }
+});
+
+document.getElementById('logout-btn').addEventListener('click', function () {
+  if (liff.isLoggedIn()) {
+    liff.logout();
+    window.location.reload();
+  }
+});
